0689c469c53e76f6b3e4e5d3502091180881d0b4
rafa <rafa.zamora.rals@gmail.com>
2019-11-15 20:46:46 -0600

    Solo sive para que el main no tenga muchas cabeceras

---
+++ PacMan/Headers/include/PacmanGame.hpp
@@ -0,0 +1,12 @@
+#ifndef PACMAN_GAME_HPP
+#define PACMAN_GAME_HPP
+
+#include "Graphics_Engine/miniwin.h"
+#include "Graphics_Engine/Cuadro.hpp"
+#include "Tablero.hpp"
+#include "Pacman.hpp"
+#include "Fantasma.hpp"
+#include "Mapas.hpp"
+#include "Comida.hpp"
+
+#endif  //PACMAN_GAME_HPP
